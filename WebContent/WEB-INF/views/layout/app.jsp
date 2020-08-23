dc508827726be1a91fe53e8d2ac13e53399c5446
yuki-hirano <yukiy99999@yahoo.co.jp>
2020-08-23 17:47:20 +0900

    新規登録画面を追加 処理は未

---
+++ WebContent/WEB-INF/views/layout/app.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>メッセージボード</title>
+    </head>
+    <body>
+        <div id="wrapper">
+            <div id="header">
+                <h1>クレイジータスク管理APP　義男</h1>
+            </div>
+            <div id="content">
+                ${param.content}
+            </div>
+            <div id="footer">
+                by バイソン小川！
+            </div>
+        </div>
+    </body>
+</html>
--- src/controlls/IndexServlet.java
+++ src/controlls/IndexServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -34,11 +35,15 @@ public class IndexServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
 
-        List<Task> messages = em.createNamedQuery("getAllTasks", Task.class)
+        List<Task> task = em.createNamedQuery("getAllTasks", Task.class)
                                    .getResultList();
-        response.getWriter().append(Integer.valueOf(messages.size()).toString());
 
         em.close();
+
+        request.setAttribute("tasks", task);
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/index.jsp");
+        rd.forward(request, response);
     }
 
 }
